8c3a66c4d49ebf4091012eb5805477bae2248c50
Asha-hash-debug <80579662+Asha-hash-debug@users.noreply.github.com>
2023-04-17 11:41:08 +0530

    Update backendservice.service.ts

--- VYOSAPP/src/app/backendservice.service.ts
+++ VYOSAPP/src/app/backendservice.service.ts
@@ -8,20 +8,17 @@ import { InterfaceName } from './parameters';
   providedIn: 'root'
 })
 export class BackendserviceService {
-  baseurl = "/"
+  baseurl = '/'
 
   constructor(private http:HttpClient) { }
-
-
 
 public InitialConfig(interfacename:any):Observable<InterfaceName>{
   return this.http.get<any>(this.baseurl+"initial-config/"+interfacename);
 }
 
-public NetworkTraffic(User): Observable<User> {
+public NetworkTraffic(User: User): Observable<User> {
     return this.http.post<any>(this.baseurl+"traffic-policy",User);
 }
-
 
 }
 
